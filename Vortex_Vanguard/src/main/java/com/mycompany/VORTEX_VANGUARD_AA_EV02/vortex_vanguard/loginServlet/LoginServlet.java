25c2e7b1e0f013ab9086b7f3318d6841e71b41be
Juan Martinez <martinezlorajuanalberto@gmail.com>
2024-05-11 18:19:39 -0500

    Subo actividad 2

---
+++ Vortex_Vanguard/src/main/java/com/mycompany/VORTEX_VANGUARD_AA_EV02/vortex_vanguard/loginServlet/LoginServlet.java
@@ -0,0 +1,84 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package com.mycompany.vortex_vanguard.loginServlet;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "LoginServlet", urlPatterns = {"/login"})
+public class LoginServlet extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+
+        Connection conexion = null;
+        Statement statement = null;
+        ResultSet rs = null;
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            String url = "jdbc:mysql://localhost:3306/vortex_1";
+            conexion = DriverManager.getConnection(url, "root", "0911");
+            statement = conexion.createStatement();
+
+            String query = "SELECT * FROM USUARIO WHERE email='" + email + "' AND password='" + password + "'";
+            rs = statement.executeQuery(query);
+
+            if (rs.next()) {
+                request.getSession().setAttribute("email", email);
+                response.sendRedirect("panel.jsp");
+            } else {
+                response.sendRedirect("index.html");
+            }
+        } catch (ClassNotFoundException | SQLException ex) {
+            Logger.getLogger(LoginServlet.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            try {
+                if (rs != null) {
+                    rs.close();
+                }
+                if (statement != null) {
+                    statement.close();
+                }
+                if (conexion != null) {
+                    conexion.close();
+                }
+            } catch (SQLException ex) {
+                Logger.getLogger(LoginServlet.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }
+}
+
---
+++ Vortex_Vanguard/src/main/webapp/panel.jsp
@@ -0,0 +1,58 @@
+<%-- 
+    Document   : panel
+    Created on : 11/05/2024, 1:52:44 a. m.
+    Author     : marti
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>VORTEX VANGUARD</title>
+         <style>
+        body {
+            font-family: Arial, sans-serif;
+          
+            margin: 0;
+            padding: 0;
+            background-image: url("vortex2.jpg");
+            background-size: cover;
+            background-repeat: no-repeat;
+            background-position: center;
+        }
+            
+        
+        .container {
+           max-width: 80%;
+            margin:50px auto;
+            padding: 20px;
+            background-color: #fff;
+            border-radius: 8px;
+            box-shadow: 0 2px 4px rgba(0,0,0,0.1);
+             display: flex;
+            flex-direction: column;
+            align-items: center;
+            opacity: 0.8;
+        h1 {
+            color: #333;
+            font-size: 36px;
+            margin-bottom: 20px;
+        }
+        p {
+            color: #333;
+            font-size: 18px;
+            line-height: 1.6;
+        }
+        .game-controller {
+            font-size: 60px;
+        }
+    </style>
+    </head>
+    <body>
+        <div class="container">
+        <h1>Bienvenido a VORTEX VANGUARD 🎮</h1>
+        <p>¡Prepárate para embarcarte en una emocionante aventura!</p>
+        </div>
+    </body>
+</html>
